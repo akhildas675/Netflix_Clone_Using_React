145817d55fc191b893d022affcd8c2e82e53f62a
AKHILDAS H <146437180+akhildas675@users.noreply.github.com>
2025-02-27 16:35:47 +0530

    Implemented navbar and banner management.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import './App.css'
+import Banner from './components/Banner/Banner';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
       <div className='App'>
         <Navbar/>
+        <Banner/>
       </div>
   );
 }
